cf16596b15cec4c23babe3645ef5f0f1d5493f5d
Ilyas Founas <ifounas@student.42lyon.fr>
2025-11-10 11:51:28 +0100

    I can push

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,6 +6,7 @@ int main(void)
     FragTrap dest("destroyer");
     ScavTrap term("terminator");
 
+    std::cout << std::endl << "DEST AND TERM TESTS :" << std::endl;
     for (int i = 0; i < 4; i++)
     {
         dest.attack("term");
@@ -13,6 +14,7 @@ int main(void)
         term.attack("dest");
         dest.takeDamage(20);
     }
+    std::cout << std::endl << "END TESTS :" << std::endl;
     dest.highFivesGuys();
     term.guardGate();
     return (0);
